5cd5547ca187099df964d025240a86791855c082
Raluca <raluca.gug@accesa.eu>
2021-02-22 12:14:49 +0200

    cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { MaterialModule } from './../modules/material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CartComponent } from './cart/cart.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -13,7 +15,10 @@ import { CartComponent } from './cart/cart.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Product } from './../models/product';
 import { Component, OnInit } from '@angular/core';
+import { FormControl , Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -7,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  products!: Product[];
+  products!: any[];
+  countVal=5;
+  selected = 'option1';
   constructor() { }
 
   ngOnInit(): void {
-    this.products=[{ name: "produs1", id: 123, image:'https://www.babyliss-romania.ro/assets/files/products/2208/fdb_1613051038_c453e_001.png', stock: 4, description: 'blaaaa blaaa', price: 20 },
-    { name: "produs1", id: 123, image: 'https://www.babyliss-romania.ro/assets/files/products/2208/fdb_1613051038_c453e_001.png', stock: 4, description: 'blaaaa blaaa', price: 25 }];
+    this.products=[{ name: "produs1", id: 123, image:'https://www.babyliss-romania.ro/assets/files/products/2208/fdb_1613051038_c453e_001.png', stock: 4, description: 'blaaaa blaaa', price: 20, qty: 1 },
+    { name: "produs1", id: 256, image: 'https://www.babyliss-romania.ro/assets/files/products/2208/fdb_1613051038_c453e_001.png', stock: 4, description: 'blaaaa blaaa', price: 25, qty:2 },
+    { name: "produs1", id: 256, image: 'https://www.babyliss-romania.ro/assets/files/products/2208/fdb_1613051038_c453e_001.png', stock: 4, description: 'blaaaa blaaa', price: 25, qty:2 },
+    { name: "produs1", id: 256, image: 'https://www.babyliss-romania.ro/assets/files/products/2208/fdb_1613051038_c453e_001.png', stock: 4, description: 'blaaaa blaaa', price: 25, qty:2 },];
   }
+  ngAfterViewInit(): void{
+    console.log(document.getElementById(`quantity${this.products[0].id}`))
+    var e=document.getElementById(`quantity${this.products[0].id}`);
+    //var result = e.options[e.selectedIndex].value;
+    this.GetSelectedValue()
+  }
+  GetSelectedValue(){
+    // var e = document.getElementById("cars");
+    // console.log("called: ",e?.value);
+    // var result = e.options[e.selectedIndex].value;
+    
+    // document.getElementById("result").innerHTML = result;
+}
+
 }
