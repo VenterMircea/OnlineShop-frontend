d67961e67f3427f97e312a9fcd618ae6cb55d99f
ovidiu <ovidiu.barbu@accesa.eu>
2021-02-22 12:07:05 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'online-store';
+  cart = [1, 2, 3, 4, 5, 6, 7];
 }
