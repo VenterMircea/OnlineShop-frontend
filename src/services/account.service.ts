8d75322d57479f79b7156f7657330ef51ac12cd9
ovidiu <ovidiu.barbu@accesa.eu>
2021-03-04 14:24:17 +0200

    comments

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -9,84 +9,87 @@ import { User } from '../app/models/user';
 
 @Injectable({ providedIn: 'root' })
 export class AccountService {
-    private userSubject!: BehaviorSubject<any>;
-    public user: Observable<any>;
-    private aux!: User;
+  private userSubject!: BehaviorSubject<any>;
+  public user: Observable<any>;
+  private aux!: User;
 
-    constructor(
-        private router: Router,
-        private http: HttpClient
-    ) { 
-        if(localStorage.getItem('user')) this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')||''));
-            else this.userSubject = new BehaviorSubject<any>(null)
-        this.user = this.userSubject.asObservable();
-    }
+  constructor(private router: Router, private http: HttpClient) {
+    if (localStorage.getItem('user'))
+      this.userSubject = new BehaviorSubject<User>(
+        JSON.parse(localStorage.getItem('user') || '')
+      );
+    else this.userSubject = new BehaviorSubject<any>(null);
+    this.user = this.userSubject.asObservable();
+  }
 
-    public get userValue(): User {
-        return this.userSubject.value;
-    }
+  public get userValue(): User {
+    return this.userSubject.value;
+  }
 
-    login(username: any, password: any) {
-        // return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
-        return this.http.post<User>(`postBeUrl`, { username, password })
-            .pipe(map(user => {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-                this.userSubject.next(user);
-                return user;
-            }));
-    }
-
-    logout() {
-        // remove user from local storage and set current user to null
-        localStorage.removeItem('user');
-        this.userSubject.next({
-            id: '',
-            username: '',
-            password: '',
-            firstName: '',
-            lastName: '',
-            token: '',
+  login(username: any, password: any) {
+    // return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+    return this.http
+      .post<User>(`postBeUrl`, { username, password })
+      .pipe(
+        map((user) => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('user', JSON.stringify(user));
+          this.userSubject.next(user);
+          return user;
         })
-        this.router.navigate(['/account/login']);
-    }
+      );
+  }
 
-    register(user: User) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
-    }
+  logout() {
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('user');
+    this.userSubject.next({
+      id: '',
+      username: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      token: '',
+    });
+    this.router.navigate(['/account/login']);
+  }
 
-    getAll() {
-        return this.http.get<User[]>(`${environment.apiUrl}/users/findAll`);
-    }
+  // register(user: User) {
+  //     return this.http.post(`${environment.apiUrl}/users/register`, user);
+  // }
 
-    getById(id: string) {
-        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
-    }
+  // getAll() {
+  //     return this.http.get<User[]>(`${environment.apiUrl}/users/findAll`);
+  // }
 
-    update(id:any, params:any) {
-        return this.http.put(`${environment.apiUrl}/users/${id}`, params)
-            .pipe(map(x => {
-                // update stored user if the logged in user updated their own record
-                if (id == this.userValue.id) {
-                    // update local storage
-                    const user = { ...this.userValue, ...params };
-                    localStorage.setItem('user', JSON.stringify(user));
+  // getById(id: string) {
+  //     return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+  // }
 
-                    // publish updated user to subscribers
-                    this.userSubject.next(user);
-                }
-                return x;
-            }));
-    }
+  // update(id:any, params:any) {
+  //     return this.http.put(`${environment.apiUrl}/users/${id}`, params)
+  //         .pipe(map(x => {
+  //             // update stored user if the logged in user updated their own record
+  //             if (id == this.userValue.id) {
+  //                 // update local storage
+  //                 const user = { ...this.userValue, ...params };
+  //                 localStorage.setItem('user', JSON.stringify(user));
 
-    delete(id: string) {
-        return this.http.delete(`${environment.apiUrl}/users/${id}`)
-            .pipe(map(x => {
-                // auto logout if the logged in user deleted their own record
-                if (id == this.userValue.id) {
-                    this.logout();
-                }
-                return x;
-            }));
-    }
+  //                 // publish updated user to subscribers
+  //                 this.userSubject.next(user);
+  //             }
+  //             return x;
+  //         }));
+  // }
+
+  // delete(id: string) {
+  //     return this.http.delete(`${environment.apiUrl}/users/${id}`)
+  //         .pipe(map(x => {
+  //             // auto logout if the logged in user deleted their own record
+  //             if (id == this.userValue.id) {
+  //                 this.logout();
+  //             }
+  //             return x;
+  //         }));
+  // }
 }
