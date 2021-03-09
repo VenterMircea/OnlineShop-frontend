e333f625336c307c71afd9b1ee098da21ee3fe55
Cristian Sita <cristian.sita@accesa.eu>
2021-03-09 16:08:54 +0000

    Pagination

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -23,80 +23,100 @@ export class ProductListComponent implements OnInit, AfterViewInit, OnDestroy {
     private searchService: SearchService,
     private scroll: ViewportScroller,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   products: any[] = [];
+  multiplePages: number[] = [];
   subscription!: Subscription;
   searchTerm = '';
   pageNumber = 0;
   totalPages = 0;
+  numberOfMultiplePages = 5;
   pageSize = 10;
+  pageSizeSelector = [10, 20, 50];
 
-  firstPage() {
+  callForProducts(): void {
+    this.productServ.getProducts(this.pageNumber, this.pageSize).subscribe((products) => {
+      this.products = products.content;
+      this.totalPages = products.totalPages;
+      this.showMultiplePages(this.multiplePages, this.pageNumber);
+    });
+    this.scroll.scrollToPosition([0, 0]);
+  }
+
+  showFirstPage(): void {
     this.pageNumber = 0;
-    this.productServ
-      .getProducts(this.pageNumber, this.pageSize)
-      .subscribe((products) => {
-        this.products = products.content;
-        this.totalPages = products.totalPages;
-      });
-    this.scroll.scrollToPosition([0, 0]);
+    this.callForProducts();
   }
 
-  decreasePageNo() {
+  decreasePageNo(): void {
     this.pageNumber--;
-    this.productServ
-      .getProducts(this.pageNumber, this.pageSize)
-      .subscribe((products) => {
-        this.products = products.content;
-        this.totalPages = products.totalPages;
-      });
-    this.scroll.scrollToPosition([0, 0]);
+    this.callForProducts();
+    console.log(this.pageNumber)
   }
 
-  increasePageNo() {
+  showPageNo(pageNo: number): void {
+    this.pageNumber = pageNo;
+    this.callForProducts();
+  }
+
+  increasePageNo(): void {
     this.pageNumber++;
-    this.productServ
-      .getProducts(this.pageNumber, this.pageSize)
-      .subscribe((products) => {
-        this.products = products.content;
-        this.totalPages = products.totalPages;
-      });
-    console.log(this.pageNumber, this.totalPages);
-    this.scroll.scrollToPosition([0, 0]);
-  }
-  lastPage() {
-    this.pageNumber = this.totalPages - 1;
-    this.productServ
-      .getProducts(this.pageNumber, this.pageSize)
-      .subscribe((products) => {
-        this.products = products.content;
-        this.totalPages = products.totalPages;
-      });
-    console.log(this.pageNumber, this.totalPages);
-    this.scroll.scrollToPosition([0, 0]);
+    this.callForProducts();
+    console.log(this.pageNumber);
   }
 
-  selectChangeHandler(event: any) {
+  showLastPage(): void {
+    this.pageNumber = this.totalPages - 1;
+    this.callForProducts();
+  }
+
+  selectPageSizeHandler(event: any): void {
     this.pageSize = event.target.value;
     this.pageNumber = 0;
-    this.productServ
-      .getProducts(this.pageNumber, this.pageSize)
-      .subscribe((products) => {
-        this.products = products.content;
-        this.totalPages = products.totalPages;
-        console.log(this.pageSize);
-      });
-    this.scroll.scrollToPosition([0, 0]);
+    this.callForProducts();
+  }
+
+  showMultiplePages([], pageNo: number): void {
+
+    let viewPages = this.totalPages - this.numberOfMultiplePages;
+
+    this.multiplePages = [];
+
+    if (this.totalPages < this.numberOfMultiplePages) {
+      pageNo = 0;
+      do {
+        pageNo++
+        this.multiplePages.push((pageNo));
+      } while (pageNo != this.totalPages)
+    }
+    else if (pageNo - 1 < viewPages) {
+      pageNo = (pageNo - Math.floor(this.numberOfMultiplePages / 2)) <= 0 ? 0
+        : (pageNo <= this.numberOfMultiplePages) ? (pageNo - Math.floor(this.numberOfMultiplePages / 2))
+          : pageNo;
+      do {
+        pageNo++
+        this.multiplePages.push((pageNo));
+      } while (this.multiplePages.length != this.numberOfMultiplePages &&
+        ([...this.multiplePages].pop() != this.totalPages))
+    }
+    else {
+      pageNo = this.totalPages - this.numberOfMultiplePages;
+      do {
+        pageNo++;
+        this.multiplePages.push((pageNo));
+      } while (this.multiplePages.length != this.numberOfMultiplePages);
+    }
   }
 
   ngOnInit() {
-    this.productServ
-      .getProducts(this.pageNumber, this.pageSize)
-      .subscribe((products) => {
-        this.products = products.content;
-        this.totalPages = products.totalPages;
-      });
+    this.productServ.getProducts(this.pageNumber, this.pageSize).subscribe((products) => {
+      this.products = products.content;
+      this.totalPages = products.totalPages;
+      this.showMultiplePages(this.multiplePages, this.pageNumber);
+      this.scroll.scrollToPosition([0, 0]);
+      console.log(products);
+    });
     this.subscription = this.searchService.currentSearchTerm.subscribe(
       (term) => {
         this.searchTerm = term;
