a5614f2fc4e2553da6c08f454afce3301809f0ec
ovidiu <ovidiu.barbu@accesa.eu>
2021-03-11 21:11:52 +0200

    user updates

--- src/app/interceptors/app-interceptor.interceptor.ts
+++ src/app/interceptors/app-interceptor.interceptor.ts
@@ -15,7 +15,7 @@ export class AppInterceptorInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    if (request.url.includes('/orders') || request.url.includes('/details')) {
+    if (request.url.includes('/orders') || request.url.includes('/users')) {
       const token = JSON.parse(localStorage.getItem('user') || '{}').token;
       const req = request.clone({
         headers: request.headers.set('Authorization', `Bearer ${token}`),
--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,12 +1,12 @@
 import { AccountService } from './../../services/account.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent implements OnInit, OnDestroy {
   user: any;
   newUser: any;
   userLogo: any;
@@ -18,27 +18,36 @@ export class UserDetailsComponent implements OnInit {
   newPassword = '';
   repeatPassword = '';
   passwordMatch = true;
+  confirm = false;
+  interval: any;
 
   enableNameForm() {
-    this.accountName = false;
+    this.accountName = !this.accountName;
   }
   enablePasswordForm() {
-    this.accountPassword = false;
+    this.accountPassword = !this.accountPassword;
   }
   enableAddressForm() {
-    this.accountAddress = false;
+    this.accountAddress = !this.accountAddress;
   }
   submitName() {
     if (this.user.password == '') {
       this.user.password = this.newUser.password;
     }
     this.accountService.userUpdate(this.user.id, this.user).subscribe();
-    console.log(this.user);
+    this.confirm = true;
+    this.interval = setInterval(() => {
+      this.confirm = false;
+    }, 1500);
   }
   submitPassword() {
     if (this.newPassword == this.repeatPassword && this.newPassword != '') {
       this.user.password = this.newPassword;
       this.accountService.userUpdate(this.user.id, this.user).subscribe();
+      this.confirm = true;
+      this.interval = setInterval(() => {
+        this.confirm = false;
+      }, 1500);
     } else {
       this.passwordMatch = false;
     }
@@ -49,6 +58,10 @@ export class UserDetailsComponent implements OnInit {
     }
     this.accountService.userUpdate(this.user.id, this.user).subscribe();
     console.log(this.user);
+    this.confirm = true;
+    this.interval = setInterval(() => {
+      this.confirm = false;
+    }, 1500);
   }
 
   ngOnInit() {
@@ -60,5 +73,10 @@ export class UserDetailsComponent implements OnInit {
     this.user = { ...this.newUser, password: '' };
     delete this.user.token;
     console.log(this.newUser);
+  }
+  ngOnDestroy() {
+    if ((this, this.interval)) {
+      clearInterval(this.interval);
+    }
   }
 }
