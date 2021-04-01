5742ce61949d2a56885d0e7ddfa52a13dc0c45c8
Cristian Sita <cristian.sita@accesa.eu>
2021-04-01 16:31:34 +0000

    fix tests coverage

--- src/app/account/register/register.component.spec.ts
+++ src/app/account/register/register.component.spec.ts
@@ -7,6 +7,8 @@ import { RegisterComponent } from './register.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AccountService } from 'src/services/account.service';
+import { CreateUser } from 'src/app/models/createUser';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -28,4 +30,28 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should check email', () => {
+    const accountService = fixture.debugElement.injector.get(AccountService);
+    component.emailExists = true;
+    component.submittedUsername = 'Nelutu';
+    component.success = 0;
+    spyOn(accountService, 'checkEmailNotTaken').and.callThrough();
+    spyOn(component, 'submit').and.callThrough();
+    component.submit();
+    component.accountService.checkEmailNotTaken("jade@email.com");
+    expect(component.submit).toHaveBeenCalled();
+    expect(component.accountService.checkEmailNotTaken).toHaveBeenCalledWith("jade@email.com");
+    expect(component.accountService.checkEmailNotTaken).toHaveBeenCalled();
+  });
+
+  it('should check username', () => {
+    component.usernameExists = true;
+    const accountService = fixture.debugElement.injector.get(AccountService);
+    spyOn(component, 'checkUsername').and.callThrough();
+    component.checkUsername();
+    component.accountService.checkUsernameNotTaken("jade");
+    //expect(component.accountService.checkUsernameNotTaken).toHaveBeenCalled();
+    expect(component.accountService.checkUsernameNotTaken).withContext('jade').toThrowError();
+  });
+
 });
