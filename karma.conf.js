a6541e45d67cde4bfc2506d6ffe34b6582492ab7
ACCESA\madalin.sava <savamadalinmihai@gmail.com>
2021-04-16 12:14:48 +0300

    added back ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -37,13 +37,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome_without_security'],
+    browsers: ['Chrome_headless','Chrome_without_security'],
     singleRun: false,
     restartOnFileChange: true,
     customLaunchers: {
       Chrome_without_security: {
         base: 'Chrome',
         flags: ['--disable-web-security', '--disable-site-isolation-trials']
+      },
+      Chrome_headless: {
+        base: 'ChromeHeadless',
+        flags: '--no-sandbox'
       }
     }
   });
